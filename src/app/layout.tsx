e4c2b7b458681453508375e4283e1da6a5b5e0fc
Jaime <jaime5alvarez5h@gmail.com>
2025-01-03 17:43:33 +0100

    Update dependencies and enhance layout with Supabase authentication
    - Added Supabase dependencies to package.json and pnpm-lock.yaml.
    - Updated layout.tsx to include Supabase client and logout functionality.
    - Modified page.tsx to change the displayed text to "hola".

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { logout } from "./login/actions";
+import { createClient } from "@/utils/supabase/server";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,16 +19,21 @@ export const metadata: Metadata = {
   description: "To Do List",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const supabase = await createClient()
+  const { data, error } = await supabase.auth.getUser();
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {data?.user && <button onClick={logout}>Logout</button>}
+
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+"use client";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">To Do List</h1>
+      hola
     </div>
   );
 }
-
