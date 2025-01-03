9c54f3e8a185aff8f9f7001b10e4bfd99dbc1aba
Jaime <jaime5alvarez5h@gmail.com>
2025-01-03 18:45:35 +0100

    Integrate Google One Tap component into login page
    - Added Google One Tap component to the login form for enhanced authentication options.
    - Minor formatting adjustments made to the login page structure.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,11 @@
 import { login, signup } from "@/app/login/actions";
+import GoogleOneTap from "@/app/components/google-one-tap";
 
 export default async function LoginPage() {
 
 
   return (
+    <>
     <form>
       <label htmlFor="email">Email:</label>
       <input id="email" name="email" type="email" required />
@@ -11,6 +13,9 @@ export default async function LoginPage() {
       <input id="password" name="password" type="password" required />
       <button formAction={login}>Log in</button>
       <button formAction={signup}>Sign up</button>
+      <GoogleOneTap />
+
     </form>
+    </>
   )
 }
